b60467a2ecae30c685afa266d799d9211d3604f6
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-29 15:04:36 +0700

    Update Commit 1

--- app/src/main/java/id/ac/polbeng/ardianto/threadexample2/MainActivity.kt
+++ app/src/main/java/id/ac/polbeng/ardianto/threadexample2/MainActivity.kt
@@ -1,20 +1,25 @@
 package id.ac.polbeng.ardianto.threadexample2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.ardianto.threadexample2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStartThread.setOnClickListener {
+            startThread()
         }
+    }
+
+    private fun startThread() {
+        val runnable = ExampleThread(10)
+        Thread(runnable).start()
     }
 }
